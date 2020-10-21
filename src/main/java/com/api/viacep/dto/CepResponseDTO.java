66644a6306a535385db26c448bbcb4debcd7b8fb
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2020-10-21 20:54:34 -0300

    VIAcepAPI

--- src/main/java/com/api/viacep/dto/CepResponseDTO.java
+++ src/main/java/com/api/viacep/dto/CepResponseDTO.java
@@ -1,28 +1,16 @@
 package com.api.viacep.dto;
 
-import com.api.viacep.model.Cep;
+import lombok.Data;
 
-
+@Data
 public class CepResponseDTO {
 
-	private long cep;
-    private String logradouro;
-    private String complemento;
-    private String bairro;
-    private String localidade;
-    private String uf;
-    private String unidade;
-    
-    public CepResponseDTO() {
-    }
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String unidade;
 
-    public CepResponseDTO(Cep cep){
-        this.cep = cep.getCep();
-        this.localidade = cep.getLocalidade();
-        this.complemento = cep.getComplemento();
-        this.bairro = cep.getBairro();
-        this.logradouro = cep.getLogradouro();
-        this.uf = cep.getUf();
-        this.unidade = cep.getUnidade();
-    }
 }
--- src/main/java/com/api/viacep/service/CepService.java
+++ src/main/java/com/api/viacep/service/CepService.java
@@ -1,40 +1,26 @@
 package com.api.viacep.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.api.viacep.dto.CepResponseDTO;
-import com.api.viacep.model.Cep;
-import com.api.viacep.repository.CepRepository;
 
 @Service
 public class CepService {
 
-	@Autowired
-	CepRepository cepRepository;
-	
-	
-	public ResponseEntity<Object> getCep(@PathVariable Cep cep){
-		Cep cepObject = cepRepository.getByCep(cep);
-		
-        if(cepObject != null) {
-            return new ResponseEntity<>(new CepResponseDTO(cepObject), HttpStatus.OK);
-        }else{
-            String url = "https://viacep.com.br/ws/"+ cep +"/json/";
-            RestTemplate restTemplate = new RestTemplate();
-            try {
-                Cep cepResponse = restTemplate.getForObject(url, Cep.class);
-                this.cepRepository.save(cepResponse);
-                return new ResponseEntity<>(new CepResponseDTO(cepResponse), HttpStatus.OK);
-            }catch (HttpClientErrorException httpClientErrorException) {
-                return new ResponseEntity<>((httpClientErrorException.getMessage()),
-                        httpClientErrorException.getStatusCode());
-            }
-        }
+	public CepResponseDTO getCep(String cep) {
+
+		String url = "https://viacep.com.br/ws/" + cep + "/json/";
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			ResponseEntity<CepResponseDTO> cepResponseDTO = restTemplate.getForEntity(url, CepResponseDTO.class);
+			return cepResponseDTO.getBody();
+
+		} catch (HttpClientErrorException httpClientErrorException) {
+			return null;
+
+		}
 	}
 }
